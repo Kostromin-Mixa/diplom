e39e68de5ba8d6544f906f0b0e245783e25fc4de
Kostromin-Mixa <78191008+Kostromin-Mixa@users.noreply.github.com>
2022-02-28 12:33:13 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,32 +2,44 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.70.0"
+
     }
+  }
+
+ backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-object"
+    region     = "us-east-1"
+    key        = "mnt/hgfs/Diplom/ya/terraform.tfstate"
+    access_key = "LSVoHA6ODhYSQfPZd1GU"
+    secret_key = "HPvWlC-paKwVhUEPwzy1C24NvhGNe1NdH2NDS_Cl"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
 }
 
 provider "yandex" {
-  token     = "AQAAAABc92bvAATuwR8yoKyMGkhwqUjs_o11cn0"
-  cloud_id  = "b1g8qt41tj2u2ts23n3v"
-  folder_id = "b1gk46nap2lt3rdlg4mf"
+  token     = "AQAAAABc5PvKAATuwcahliVymEgIibAj27msSvs"
+  cloud_id  = "b1gie36vbchrk9je19j3"
+  folder_id = "b1ggj97b2ktqrl7rkcau"
   zone      = "ru-central1-a"
 }
 
 locals {
-  folder_id = "b1gk46nap2lt3rdlg4mf"
+  folder_id = "b1ggj97b2ktqrl7rkcau"
 }
 
-// Создание сервис аккаунта terraform
+// Создание сервис аккаунта kubernetes
 resource "yandex_iam_service_account" "sa" {
   folder_id = local.folder_id
-  name      = "terraform"
-}
+  name      = "kub"
+ }
 
 // Назначение роли
 resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
   folder_id = local.folder_id
-  role      = "admin"
+  role      = "editor"
   member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
 
@@ -35,156 +47,131 @@ resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
 resource "yandex_vpc_network" "network" {
   name = "diplom"
 }
-resource "yandex_vpc_subnet" "public-subnet" {
-  name           = "public"
+resource "yandex_vpc_subnet" "subnet-kube-a" {
+  name           = "private-kube-a"
   v4_cidr_blocks = ["192.168.10.0/24"]
   zone           = "ru-central1-a"
-  description    = "NAT instance"
+  description    = "Private instance"
   network_id     = yandex_vpc_network.network.id
 }
- resource "yandex_vpc_subnet" "private-subnet1" {
-  name           = "private1"
+resource "yandex_vpc_subnet" "subnet-kube-b" {
+  name           = "private-kube-b"
   v4_cidr_blocks = ["192.168.20.0/24"]
-  zone           = "ru-central1-a"
+  zone           = "ru-central1-b"
   description    = "Private instance"
   network_id     = yandex_vpc_network.network.id
 }
- resource "yandex_vpc_subnet" "private-subnet2" {
-  name           = "private2"
+resource "yandex_vpc_subnet" "subnet-kube-c" {
+  name           = "private-kube-c"
   v4_cidr_blocks = ["192.168.30.0/24"]
-  zone           = "ru-central1-a"
-  description    = "Private instance"
-  network_id     = yandex_vpc_network.network.id
-}
-resource "yandex_vpc_subnet" "private-subnet3" {
-  name           = "private3"
-  v4_cidr_blocks = ["192.168.40.0/24"]
-  zone           = "ru-central1-a"
+  zone           = "ru-central1-c"
   description    = "Private instance"
   network_id     = yandex_vpc_network.network.id
 }
 
-// Создание виртуальных машин
-resource "yandex_compute_instance" "master" {
-  name        = "master"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-  
-  resources {
-    core_fraction = 20
-    cores  = 2
-    memory = 2
-  }
+// Создание регионального мастера kubernetes
+resource "yandex_kubernetes_cluster" "regional_cluster_resource_kuber" {
+  name        = "my-kuber"
+  description = "regional cluster"
 
-  boot_disk {
-    initialize_params {
-      image_id = "fd8mfc6omiki5govl68h"
-      size = 15
+  network_id = yandex_vpc_network.network.id
+
+  master {
+    regional {
+      region = "ru-central1"
+
+      location {
+        zone      = yandex_vpc_subnet.subnet-kube-a.zone
+        subnet_id = yandex_vpc_subnet.subnet-kube-a.id
+      }
+
+      location {
+        zone      = yandex_vpc_subnet.subnet-kube-b.zone
+        subnet_id = yandex_vpc_subnet.subnet-kube-b.id
+      }
+
+      location {
+        zone      = yandex_vpc_subnet.subnet-kube-c.zone
+        subnet_id = yandex_vpc_subnet.subnet-kube-c.id
+      }
+    }
+
+    version   = "1.20"
+    public_ip = true
+
+    maintenance_policy {
+      auto_upgrade = true
+
+      maintenance_window {
+        day        = "monday"
+        start_time = "15:00"
+        duration   = "3h"
+      }
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.public-subnet.id
-    nat       = true
-    ip_address = "192.168.10.10"
+  service_account_id      = yandex_iam_service_account.sa.id
+  node_service_account_id = yandex_iam_service_account.sa.id
+
+  labels = {
+    my_key       = "value"
+    my_other_key = "other_value"
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-
+  release_channel = "STABLE"
 }
 
-resource "yandex_compute_instance" "worker1" {
-  name        = "worker1"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-  
-  resources {
-    core_fraction = 5
-    cores  = 2
-    memory = 2
+// Создание группы узлов
+resource "yandex_kubernetes_node_group" "my_node_group" {
+  cluster_id  = yandex_kubernetes_cluster.regional_cluster_resource_kuber.id
+  name        = "kube-group"
+  description = "description"
+  version     = "1.20"
+
+  labels = {
+    "key" = "value"
   }
 
-  boot_disk {
-    initialize_params {
-      image_id = "fd8mfc6omiki5govl68h"
-      size = 15
+  instance_template {
+    platform_id = "standard-v2"
+
+    network_interface {
+      nat                = true
+      subnet_ids         = ["${yandex_vpc_subnet.subnet-kube-a.id}"]
+    }
+    resources {
+      memory = 2
+      cores  = 2
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 30
+    }
+
+    scheduling_policy {
+      preemptible = true
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.private-subnet1.id
-    nat       = true
-    ip_address = "192.168.20.20"
-  }
-
-  metadata = {
-     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
- }
-
-resource "yandex_compute_instance" "worker2" {
-  name        = "worker2"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-  
-  resources {
-    core_fraction = 5
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8mfc6omiki5govl68h"
-      size = 15
+  scale_policy {
+    auto_scale {
+      min     = 2
+      max     = 3
+      initial = 3
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.private-subnet2.id
-    nat       = true
-    ip_address = "192.168.30.30"
-  }
+  allocation_policy {
+    location {
+      zone = "ru-central1-a"
 
-  metadata = {
-     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
- }
-
-resource "yandex_compute_instance" "worker3" {
-  name        = "worker3"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-  
-  resources {
-    core_fraction = 5
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8mfc6omiki5govl68h"
-      size = 15
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.private-subnet3.id
-    nat       = true
-    ip_address = "192.168.40.40"
+  maintenance_policy {
+    auto_upgrade = true
+    auto_repair  = true
+
+    }
   }
-
-  metadata = {
-     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
- }
-
-
-
-
-
-
-
